31307471fa2fea944b9aec69f7123ede091f44be
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-05-28 15:40:20 +0300

    Add Appbar widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   home: Scaffold(
     backgroundColor: Colors.red,
-    appBar: AppBar()
+    appBar: AppBar(
+      backgroundColor: Colors.red,
+      title: Text("Dicee"),
+    )
   ),
 ));
