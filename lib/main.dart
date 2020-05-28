24d696535d7933a0782041ffb0550b1491b68d5f
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-05-28 15:42:03 +0300

    Add custom dice body widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    backgroundColor: Colors.red,
-    appBar: AppBar(
-      backgroundColor: Colors.red,
-      title: Text("Dicee"),
-    )
-  ),
-));
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text("Dicee"),
+        ),
+        body: DiceBody(),
+      ),
+    ));
+
+class DiceBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
